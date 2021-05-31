ed11be80f0c5ca36a5819763fadc41cd85709b53
nichro02 <nichro02@gmail.com>
2021-05-31 14:09:38 -0600

    set up cors and json middleware

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+
+//middleware
+//allow cors
+app.use(cors())
+//allow us to use req.body to get json data
+app.use(express.json())
 
 app.listen(5000, () => {
     console.log('listening on port 5000')
