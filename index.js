42c21e57df3e6d97d16c6bdb6f816fc01830f5a4
nichro02 <nichro02@gmail.com>
2021-06-07 19:31:11 -0600

    Refined create todo route to return only created todo description in response

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ app.use(express.json())
 app.post('/todos', async(req, res)=>{
     try {
         const { description } = req.body
-        const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1)', [description])
+        const newTodo = await pool.query('INSERT INTO todo (description) VALUES($1) RETURNING *', [description])
 
-        res.json(newTodo)
+        res.json(newTodo.rows[0])
     } catch (error) {
         console.log(error.message)
     }
